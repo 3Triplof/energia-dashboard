52be65019950343031c94a1ab5bddbd26afb37e7
mendes <bolaoprimos2022@gmail.com>
2026-05-18 17:01:21 -0300

    Add function to load data from Google Sheets
    Implement function to load data from Google Sheets into a DataFrame.

---
+++ services/sheets.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+def carregar_dados():
+
+    creds_dict = dict(st.secrets["gcp_service_account"])
+
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive"
+    ]
+
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+        creds_dict,
+        scope
+    )
+
+    client = gspread.authorize(creds)
+
+    planilha = client.open("Energia")
+
+    aba = planilha.sheet1
+
+    dados = aba.get_all_records()
+
+    df = pd.DataFrame(dados)
+
+    return df

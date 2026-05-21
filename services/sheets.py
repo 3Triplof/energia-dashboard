37f78bb06e996fc91d53affab5012449b586f672
mendes <bolaoprimos2022@gmail.com>
2026-05-21 08:42:31 -0300

    Update sheets.py

--- services/sheets.py
+++ services/sheets.py
@@ -1,30 +1,59 @@
 import streamlit as st
 import gspread
 import pandas as pd
-from oauth2client.service_account import ServiceAccountCredentials
+
+from oauth2client.service_account import (
+    ServiceAccountCredentials
+)
+
+
+# =========================
+# CARREGAR DADOS
+# =========================
 
 def carregar_dados():
 
-    creds_dict = dict(st.secrets["gcp_service_account"])
+    creds_dict = dict(
+        st.secrets["gcp_service_account"]
+    )
 
     scope = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
 
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(
-        creds_dict,
-        scope
+    creds = (
+        ServiceAccountCredentials
+        .from_json_keyfile_dict(
+            creds_dict,
+            scope
+        )
     )
 
     client = gspread.authorize(creds)
 
-    planilha = client.open("Energia")
+    planilha = client.open(
+        "Energia"
+    )
 
     aba = planilha.sheet1
 
-    dados = aba.get_all_records()
+    # =========================
+    # PEGA VALORES FORMATADOS
+    # =========================
 
-    df = pd.DataFrame(dados)
+    dados = aba.get_all_values()
+
+    # Cabeçalho
+    colunas = dados[0]
+
+    # Dados
+    linhas = dados[1:]
+
+    # DataFrame
+    df = pd.DataFrame(
+        linhas,
+        columns=colunas
+    )
 
     return df
